c18a272203a97c60d3fc6be480ac184757ed9143
Alejandro Garau Madrigal <alegarmad@uma.es>
2020-05-31 17:42:49 +0200

    Verga

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -10,18 +10,37 @@ public class PlayerMovement : MonoBehaviour
 
     private NavMeshAgent navmesh;
     private NavMeshPath path;
+    private int pathIndex; 
+    private Rigidbody Rigidbody;
+    public float Thrust;
 
     // Start is called before the first frame update
     void Start()
     {
+        Rigidbody = GetComponent<Rigidbody>();
         navmesh = GetComponent<NavMeshAgent>();
         path = new NavMeshPath();
+        pathIndex = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log(path.corners.Length);
+    }
+
+    private void FixedUpdate()
+    {
+        var currentPosition = transform.position;
+        var positionToMove = path.corners[pathIndex];
+        var directionVector = new Vector3(currentPosition.x - positionToMove.x, currentPosition.y - positionToMove.y);
+
+        Rigidbody.AddForce(directionVector * Thrust);
+
+        if (currentPosition == positionToMove)
+        {
+            pathIndex++;
+        }
     }
 
     public void ChaseBall(GameObject ball)
