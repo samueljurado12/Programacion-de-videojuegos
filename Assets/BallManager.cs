cedf99ea54da5423668ef8b09851603839a3c949
Samuel Jurado Quintana <samueljurado12@gmail.com>
2020-05-31 13:46:29 +0200

    Ball Respawn
    Added a Ball spawner manager to create new balls after scoring.
    Added a new basket model with trigger

---
+++ Assets/BallManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallManager : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3[] spawnPositions;
+    [SerializeField]
+    private Transform character;
+    [SerializeField]
+    private GameObject ball;
+
+    public void SpawnNewBall()
+    {
+        Vector3 spawnPos = spawnPositions[Random.Range(0, spawnPositions.Length)];
+        GameObject newBall = Instantiate(ball, spawnPos, Quaternion.identity);
+        //character.BallToChase = newBall;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(spawnPositions.Length > 0)
+        {
+            foreach (Vector3 vector in spawnPositions)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawSphere(vector, 0.5f);
+            }
+        }
+    }
+}
---
+++ Assets/Basket.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket : MonoBehaviour
+{
+
+    [SerializeField]
+    private BallManager ballManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            Debug.Log("TRES PUNTOS COLEGA");
+            Destroy(other.gameObject);
+            ballManager.SpawnNewBall();
+        }
+    }
+}
